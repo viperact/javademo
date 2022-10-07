e9750383e14ce5feb42273bd2637bda7e9b0214f
lsh <viperact26@gmail.com>
2022-10-07 18:03:46 +0900

    update java

--- src/java004_array/answ/Prob_01.java
+++ src/java004_array/answ/Prob_01.java
@@ -1,4 +1,4 @@
-package java004_array.prob;
+package java004_array.answ;
 
 /*
  * num배열에서 최대값을 출력하는 프로그램을 구현하시오.
@@ -10,10 +10,14 @@ public class Prob_01 {
 	public static void main(String[] args) {
 
 		int[] num = { 94, 85, 95, 88, 90 };
-		
-		//여기를 구현하세요.
-		for(int i = 0 ; i < num.length; i++);
-			
+		int max = num[0];
+		for (int i = 1; i < num.length; i++) {
+			if (max < num[i]) {
+				max = num[i];
+			}
+		}
+		System.out.println("최대값:" + max);
+
 	}// end main()
 
 }// end class
