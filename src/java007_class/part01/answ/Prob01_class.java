7d875abc3610ce7c8e56c936393a68cf41d22de7
lsh <viperact26@gmail.com>
2022-10-13 18:19:43 +0900

    update java study

--- src/java007_class/part01/answ/Prob01_class.java
+++ src/java007_class/part01/answ/Prob01_class.java
@@ -1,4 +1,4 @@
-package java007_class.part01.prob;
+package java007_class.part01.answ;
 
 /* 출력력과처럼 총합계까지 출력이 되도록 main()메소드에 
  * 로직을 추가하세요.
@@ -10,6 +10,7 @@ package java007_class.part01.prob;
  * 총합계: 50500
  */
 
+
 public class Prob01_class {
 
 	public static void main(String[] args) {
@@ -18,18 +19,17 @@ public class Prob01_class {
 		me[0] = new MovieShop("연평해전", 7000, 2);
 		me[1] = new MovieShop("극비수사", 7500, 3);
 		me[2] = new MovieShop("소수의견", 7000, 2);
-		
-		
-		// 여기에 구현하세요/////////////////
 
+		// 여기에 구현하세요/////////////////
 		for (int i = 0; i < me.length; i++) {
 			me[i].prn();
-			
-			
+			sum += me[i].countMoney();
 		}
-
+		System.out.println("총합계: " + sum);
+		
 		//////////////////////////
 
 	}// end main()
 
 }// end class
+
