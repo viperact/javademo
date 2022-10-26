51717a4bc3c96080042a1a50a83e0df276f524fe
lsh <viperact26@gmail.com>
2022-10-26 18:17:03 +0900

    update

---
+++ src/java020_thread/prob/Consumer.java
@@ -0,0 +1,13 @@
+package java020_thread.prob;
+
+public class Consumer extends Thread{
+	
+	public Consumer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Consumer(VendingMachine vm) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/java020_thread/prob/Producer.java
@@ -0,0 +1,15 @@
+package java020_thread.prob;
+
+public class Producer extends Thread {
+	
+	public Producer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Producer(VendingMachine vm) {
+		
+	}
+
+	
+
+}
